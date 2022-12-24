1f2f95ba2e6d5a12dada78287f8419ea6336e087
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2022-12-24 17:41:43 +0100

    feat(localstorage): Add LocalStorage to the common lib

--- projects/the-one-portal-shell/src/public-api.ts
+++ projects/the-one-portal-shell/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/the-one-portal-shell.service';
 export * from './lib/the-one-portal-shell.component';
 export * from './lib/the-one-portal-shell.module';
 export * from './lib/auth/index';
+export * from './lib/shared/services/local-storage.service';
